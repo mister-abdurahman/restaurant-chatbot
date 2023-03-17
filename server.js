31518d0dd7983bc6c78e1ff5232600390e08bd8d
Abdurahman Aramide <aramramadan7@gmail.com>
2023-03-17 11:03:39 +0100

    enabled sessions

--- server.js
+++ server.js
@@ -3,34 +3,72 @@ const session = require("express-session");
 const app = express();
 require("./db").connectToMongoDB();
 require("dotenv").config();
+
+const MongoDBStore = require("connect-mongodb-session")(session);
+
+const store = new MongoDBStore({
+  uri: process.env.MONGO_DB_URL_CONNECTION,
+  collection: "mySessions",
+});
+
+// Catch errors
+store.on("error", function (error) {
+  console.log(error);
+});
+
 const itemDB = require("./model/itemSchema");
 //
 const PORT = process.env.PORT || 3000;
 const http = require("http").Server(app); //binding our app to the http module
 
+// Session config
+const sessionMiddleware = session({
+  secret: process.env.SECRET,
+  resave: true,
+  cookie: { maxAge: 10519200000 },
+  store: store,
+  saveUninitialized: true,
+});
+
+app.use(sessionMiddleware);
+
 // attach http server to socket.io
 const io = require("socket.io")(http);
+
+io.use((socket, next) => {
+  sessionMiddleware(socket.request, {}, next);
+});
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
-// app.use(session({
-//   secret: process.env.SECRET,
-//   resave: false,
-//   saveUninitialized: false
-// }))
 
-let orderedItemsArray = [];
+// let orderedItemsArray = [];
 
 // create a new connection from the server side
 io.on("connection", async (socket) => {
-  //   console.log("A user connected");
+  const req = socket.request.session;
 
-  const database = await itemDB.find({});
-  const dateObj = new Date();
-  const month = dateObj.getUTCMonth() + 1; //months from 1-12
-  const day = dateObj.getUTCDate();
-  const year = dateObj.getUTCFullYear();
+  console.log("A user connected");
+  console.log(req.id);
+  console.log(req.userId);
+  req.userId = Math.random();
+  req.save();
+  console.log(req.userId);
+
+  // const database = await itemDB.find({});
+  // const dateObj = new Date();
+  // const month = dateObj.getUTCMonth() + 1; //months from 1-12
+  // const day = dateObj.getUTCDate();
+  // const year = dateObj.getUTCFullYear();
+
+  req.database = await itemDB.find({});
+  req.orderedItemsArray = [];
+  req.month = new Date().getUTCMonth() + 1;
+  req.day = new Date().getUTCDate();
+  req.year = new Date().getUTCFullYear();
+
+  req.save();
 
   socket.on("disconnect", () => {
     console.log("A user disconnected");
@@ -38,44 +76,59 @@ io.on("connection", async (socket) => {
   socket.on("items_to_order", (data) => {
     if (data === 1) {
       console.log(data);
-      socket.emit("the_items_to_order", database);
+      // socket.emit("the_items_to_order", database);
+      socket.emit("the_items_to_order", req.database);
     }
   });
   socket.on("items_ordered", (itemName) => {
     console.log(itemName);
-    orderedItemsArray.push({
+    // orderedItemsArray.push({
+    req.orderedItemsArray.push({
       name: itemName,
-      time: day + "/" + month + "/" + year,
+      // time: day + "/" + month + "/" + year,
+      time: req.day + "/" + req.month + "/" + req.year,
       orderStatus: "added to cart",
     });
+    req.save();
   });
 
   socket.on("completed_orders", (orderCompleted) => {
-    orderedItemsArray.forEach((element) => {
+    // orderedItemsArray.forEach((element) => {
+    req.orderedItemsArray.forEach((element) => {
       element.orderStatus = orderCompleted;
     });
-    socket.emit("ordered_items_array", orderedItemsArray);
+    req.save();
+    // socket.emit("ordered_items_array", orderedItemsArray);
+    socket.emit("ordered_items_array", req.orderedItemsArray);
   });
 
   socket.on("generate_completed_orders", (aBoolean) => {
-    let newArr = new Array(...orderedItemsArray)
-    socket.emit("completed_orders_list", newArr);
+    // let newArr = new Array(...orderedItemsArray);
+    req.newArr = new Array(...req.orderedItemsArray);
+    req.save();
+    // socket.emit("completed_orders_list", newArr);
+    socket.emit("completed_orders_list", req.newArr);
     if (aBoolean === true) {
-      newArr = [];
+      req.newArr = [];
       aBoolean = false;
+      req.save();
     }
   });
 
   socket.on("current_order", (currentOrder) => {
     socket.emit(
       "current_order_item",
-      orderedItemsArray[orderedItemsArray.length - 1]
+      // orderedItemsArray[orderedItemsArray.length - 1]
+      req.orderedItemsArray[req.orderedItemsArray.length - 1]
     );
+    req.save();
   });
 
   socket.on("emit_to_cancel_order", (data) => {
-    const lastData = orderedItemsArray.pop();
-    socket.emit("cancel_order", lastData);
+    // const lastData = orderedItemsArray.pop();
+    req.lastData = req.orderedItemsArray.pop();
+    req.save();
+    socket.emit("cancel_order", req.lastData);
   });
 });
 
